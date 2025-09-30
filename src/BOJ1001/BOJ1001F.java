f3769b3bdb842356d27237937619586160a223fc
developerjjoa <developerjjoa@gmail.com>
2025-09-30 13:24:29 +0900

    BOJ1001F Scanner O(1)

---
+++ src/BOJ1001/BOJ1001F.java
@@ -0,0 +1,12 @@
+package BOJ1001;
+
+import java.util.Scanner;
+
+public class BOJ1001F {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A - B);
+    }
+}
