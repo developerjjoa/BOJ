6cf708d5bcc77af08b7253206bc3a497a51116ee
developerjjoa <developerjjoa@gmail.com>
2025-10-01 13:56:59 +0900

    BOJ1008B Scanner + printf O(1)

---
+++ src/BOJ1008/BOJ1008B.java
@@ -0,0 +1,12 @@
+package BOJ1008;
+
+import java.util.Scanner;
+
+public class BOJ1008B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        System.out.printf("%.9f", A / B);
+    }
+}
