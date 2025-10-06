96523ddbb2a34f92e0094256eb3d52a7cefefd02
developerjjoa <developerjjoa@gmail.com>
2025-10-06 17:56:59 +0900

    BOJ10951E1 Scanner + while 반복문(hasNextInt()) + 마지막에 Scanner 닫아주기 O(N)

---
+++ src/BOJ10951/BOJ10951E1.java
@@ -0,0 +1,16 @@
+package BOJ10951;
+
+import java.util.Scanner;
+
+public class BOJ10951E1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextInt()) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            System.out.println(A + B);
+        }
+        scanner.close();
+    }
+}
