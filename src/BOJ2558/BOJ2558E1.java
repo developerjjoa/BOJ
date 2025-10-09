544658c17c8dc4ec70a03368fbbcb5b87b2d326f
developerjjoa <developerjjoa@gmail.com>
2025-10-09 12:43:46 +0900

    BOJ2558E1 Scanner O(1)

---
+++ src/BOJ2558/BOJ2558E1.java
@@ -0,0 +1,13 @@
+package BOJ2558;
+
+import java.util.Scanner;
+
+public class BOJ2558E1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+        scanner.close();
+    }
+}
