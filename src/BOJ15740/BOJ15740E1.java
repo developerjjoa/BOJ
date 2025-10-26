7372b822ad9d576a62af7edc42718818817916d1
developerjjoa <developerjjoa@gmail.com>
2025-10-26 15:29:36 +0900

    BOJ15740E1 Scanner + BigInteger(sc.nextBigInteger()) O(N)

---
+++ src/BOJ15740/BOJ15740E1.java
@@ -0,0 +1,14 @@
+package BOJ15740;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ15740E1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        System.out.println(A.add(B));
+        sc.close();
+    }
+}
