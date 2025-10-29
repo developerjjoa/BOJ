7b9a7e0e1a9e280be6fb33b2b40b95f1038328ca
developerjjoa <developerjjoa@gmail.com>
2025-10-29 15:32:36 +0900

    BOJ1330E2 Scanner + if문 O(1)

---
+++ src/BOJ1330/BOJ1330E2.java
@@ -0,0 +1,21 @@
+package BOJ1330;
+
+import java.util.Scanner;
+
+public class BOJ1330E2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+
+        sc.close();
+    }
+}
