e62e2f2d5133b743d91977a4bfdede51f4a03b19
developerjjoa <developerjjoa@gmail.com>
2025-11-07 21:23:30 +0900

    BOJ2557E1 BufferedWriter(write,flush,close) O(1)

---
+++ src/BOJ2557/BOJ2557E1.java
@@ -0,0 +1,14 @@
+package BOJ2557;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ2557E1 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+}
