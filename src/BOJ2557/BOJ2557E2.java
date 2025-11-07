657263478e6aee3924ff6d068c6c3ba5aca76748
developerjjoa <developerjjoa@gmail.com>
2025-11-07 21:29:38 +0900

    BOJ2557E2 StringBuilder(append) O(1)

---
+++ src/BOJ2557/BOJ2557E2.java
@@ -0,0 +1,9 @@
+package BOJ2557;
+
+public class BOJ2557E2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.println(sb.toString());
+    }
+}
